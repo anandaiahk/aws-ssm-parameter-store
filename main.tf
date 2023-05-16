7e48a151e0f3ef380f6ae3f2c6e0f897fa9237ce
anandk <kanandaiah9@gmail.com>
2023-05-16 21:35:24 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test1"
   value = "hello world"
-  type  = "securestring"
+  type  = "SecureString"
   key_id = "21227fb0-50f5-4349-a0d1-794f9ba10068"
 }
